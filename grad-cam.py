7883e94c1b205d4f02a19b01bc9c04f4cfd6e72b
Kevin Qi <qiwen@qiwen.name>
2020-03-20 21:59:10 +0800

    support resnet

--- grad-cam.py
+++ grad-cam.py
@@ -1,5 +1,6 @@
 import argparse
 import cv2
+import json
 import numpy as np
 import torch
 from torch.autograd import Function
@@ -9,8 +10,10 @@ class FeatureExtractor():
     """ Class for extracting activations and 
     registering gradients from targetted intermediate layers """
 
-    def __init__(self, model, target_layers):
+    def __init__(self, model, pre_features, features, target_layers):
         self.model = model
+        self.pre_features = pre_features
+        self.features = features
         self.target_layers = target_layers
         self.gradients = []
 
@@ -20,7 +23,14 @@ class FeatureExtractor():
     def __call__(self, x):
         outputs = []
         self.gradients = []
-        for name, module in self.model._modules.items():
+        
+        for pref in self.pre_features:
+            x = getattr(self.model, pref)(x)
+
+        submodel = getattr(self.model, self.features)
+        # go through the feature extractor's forward pass
+        for name, module in submodel._modules.items():
+            # print(name, module)
             x = module(x)
             if name in self.target_layers:
                 x.register_hook(self.save_gradient)
@@ -34,17 +44,36 @@ class ModelOutputs():
     2. Activations from intermeddiate targetted layers.
     3. Gradients from intermeddiate targetted layers. """
 
-    def __init__(self, model, target_layers):
+    def __init__(self, model,
+                 pre_feature_block=[],
+                 feature_block='features',
+                 target_layers='35',
+                 classifier_block=['classifier']):
         self.model = model
-        self.feature_extractor = FeatureExtractor(self.model.features, target_layers)
+        self.classifier_block = classifier_block
+        # assume the model has a module named `feature`      ⬇⬇⬇⬇⬇⬇⬇⬇
+        self.feature_extractor = FeatureExtractor(self.model,
+                                                  pre_feature_block,
+                                                  feature_block,
+                                                  target_layers)
 
     def get_gradients(self):
         return self.feature_extractor.gradients
 
     def __call__(self, x):
+        # ⬇ target layer    ⬇ final layer's output
         target_activations, output = self.feature_extractor(x)
-        output = output.view(output.size(0), -1)
-        output = self.model.classifier(output)
+        print('target_activations[0].size: {}'.format(target_activations[0].size()))# for vgg'35 ([1, 512, 14, 14])
+        print('output.size: {}'.format(output.size()))                              # for vgg'36 ([1, 512, 7, 7])
+
+        for i, classifier in enumerate(self.classifier_block):
+            if i == len(self.classifier_block) - 1:
+                output = output.view(output.size(0), -1)
+                print('output.view.size: {}'.format(output.size()))                 # for vgg'36 ([1, 25088])
+
+            output = getattr(self.model, classifier)(output)
+            print('output.size: {}'.format(output.size()))                          # for vgg'36 ([1, 1000])
+
         return target_activations, output
 
 
@@ -73,14 +102,21 @@ def show_cam_on_image(img, mask):
 
 
 class GradCam:
-    def __init__(self, model, target_layer_names, use_cuda):
+    def __init__(self, model, pre_feature_block, feature_block, target_layer_names, classifier_block, use_cuda):
         self.model = model
         self.model.eval()
+        self.pre_feature_block = pre_feature_block
+        self.feature_block = feature_block
+        self.classifier_block = classifier_block
         self.cuda = use_cuda
         if self.cuda:
             self.model = model.cuda()
 
-        self.extractor = ModelOutputs(self.model, target_layer_names)
+        self.extractor = ModelOutputs(self.model,
+                                      pre_feature_block,
+                                      feature_block,
+                                      target_layer_names,
+                                      classifier_block)
 
     def forward(self, input):
         return self.model(input)
@@ -94,6 +130,11 @@ class GradCam:
         if index == None:
             index = np.argmax(output.cpu().data.numpy())
 
+        with open('imagenet_class_index.json') as f:
+            labels = json.load(f)
+        
+        print('prediction[{}]: {}'.format(index, labels[str(index)][1]))
+        print('output.size: {}'.format(output.size()))                            # for vgg'36 ([1, 1000])
         one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
         one_hot[0][index] = 1
         one_hot = torch.from_numpy(one_hot).requires_grad_(True)
@@ -102,23 +143,37 @@ class GradCam:
         else:
             one_hot = torch.sum(one_hot * output)
 
-        self.model.features.zero_grad()
-        self.model.classifier.zero_grad()
+        #print('output: {}'.format(output))
+        #print('one_hot: {}'.format(one_hot))                                      # 
+        getattr(self.model, self.feature_block).zero_grad()
+        for classifier in self.classifier_block:
+            getattr(self.model, classifier).zero_grad()
         one_hot.backward(retain_graph=True)
 
+        gradients = self.extractor.get_gradients()
+        #print('len(gradients): {}'.format(len(gradients)))
+        print('gradients[0].size(): {}'.format(gradients[0].size()))
         grads_val = self.extractor.get_gradients()[-1].cpu().data.numpy()
 
         target = features[-1]
+        print('target.size(): {}'.format(target.size()))
         target = target.cpu().data.numpy()[0, :]
+        print('target.shape: {}'.format(target.shape))
 
         weights = np.mean(grads_val, axis=(2, 3))[0, :]
+        print('weights.shape: {}'.format(weights.shape))
         cam = np.zeros(target.shape[1:], dtype=np.float32)
+        print('cam.shape: {}'.format(cam.shape))            # (14, 14)
 
         for i, w in enumerate(weights):
             cam += w * target[i, :, :]
-
-        cam = np.maximum(cam, 0)
+        
+        #print('cam: {}'.format(cam))
+        print('cam.shape: {}'.format(cam.shape))
+        cam = np.maximum(cam, 0)                            # remove negative numbers
         cam = cv2.resize(cam, (224, 224))
+        print('cam.shape: {}'.format(cam.shape))
+        #print('cam: {}'.format(cam))
         cam = cam - np.min(cam)
         cam = cam / np.max(cam)
         return cam
@@ -180,8 +235,6 @@ class GuidedBackpropReLUModel:
         else:
             one_hot = torch.sum(one_hot * output)
 
-        # self.model.features.zero_grad()
-        # self.model.classifier.zero_grad()
         one_hot.backward(retain_graph=True)
 
         output = input.grad.cpu().data.numpy()
@@ -223,26 +276,52 @@ if __name__ == '__main__':
     and computes intermediate activations.
     Makes the visualization. """
 
-    args = get_args()
+    image_path = './examples/wolf.png'
+    use_cuda = False
 
     # Can work with any model, but it assumes that the model has a
     # feature method, and a classifier method,
     # as in the VGG models in torchvision.
-    grad_cam = GradCam(model=models.vgg19(pretrained=True), \
-                       target_layer_names=["35"], use_cuda=args.use_cuda)
+    config = {
+        'vgg19':    {
+            'pre_feature': [],
+            'features': 'features',
+            'target': ['35'],
+            'classifier': ['classifier']
+        }, 
+        'resnet50': {
+            'pre_feature': ['conv1', 'bn1', 'relu', 'maxpool', 'layer1', 'layer2', 'layer3'],
+            'features': 'layer4',
+            'target': ['2'],
+            'classifier': ['avgpool', 'fc']
+        }
+    }
 
-    img = cv2.imread(args.image_path, 1)
+    model_name = 'resnet50'
+    config = config[model_name]
+    model = getattr(models, model_name)(pretrained=True)
+    grad_cam = GradCam(model,
+                       pre_feature_block=config['pre_feature'],
+                       feature_block=config['features'], # features
+                       target_layer_names=config['target'],
+                       classifier_block=config['classifier'],  # classifier
+                       use_cuda=use_cuda)
+
+    img = cv2.imread(image_path, 1)
     img = np.float32(cv2.resize(img, (224, 224))) / 255
+
+    #print('img.size(): {}'.format(img.size))
     input = preprocess_image(img)
+    #print('input.size(): {}'.format(input.size()))
 
     # If None, returns the map for the highest scoring category.
     # Otherwise, targets the requested index.
     target_index = None
     mask = grad_cam(input, target_index)
-
+    
     show_cam_on_image(img, mask)
-
-    gb_model = GuidedBackpropReLUModel(model=models.vgg19(pretrained=True), use_cuda=args.use_cuda)
+    '''
+    gb_model = GuidedBackpropReLUModel(model=models.vgg19(pretrained=True), use_cuda=use_cuda)
     gb = gb_model(input, index=target_index)
     gb = gb.transpose((1, 2, 0))
     cam_mask = cv2.merge([mask, mask, mask])
@@ -251,3 +330,4 @@ if __name__ == '__main__':
 
     cv2.imwrite('gb.jpg', gb)
     cv2.imwrite('cam_gb.jpg', cam_gb)
+    '''
